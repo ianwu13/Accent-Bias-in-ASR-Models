5ab10e2bff6bacfe60baaf7647db7ef9395e3c6d
Ian Wu <ianwu13@me.com>
2024-03-16 13:33:16 -0700

    add self to method params

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -11,7 +11,7 @@ class Evaluator():
             self.bertscore_model = load('bertscore')
 
 
-    def wer(pred: str|list, ref: str|list) -> float:
+    def wer(self, pred: str|list, ref: str|list) -> float:
         # Returns word error rate (WER) between some predicted text and a reference (true label) text
         if type(pred) == str and type(ref) == str:
             return wer(ref, pred)
@@ -21,7 +21,7 @@ class Evaluator():
             return None
 
 
-    def cer(pred: str|list, ref: str|list) -> float:
+    def cer(self, pred: str|list, ref: str|list) -> float:
         # Returns character error rate (CER) between some predicted text and a reference (true label) text
         if self.cer_metric is None:
             self.cer_metric = load('cer')
@@ -34,7 +34,7 @@ class Evaluator():
             return None
 
 
-    def bertscore(pred: str|list, ref: str|list) -> float:
+    def bertscore(self, pred: str|list, ref: str|list) -> float:
         # Returns bertscore between some predicted text and a reference (true label) text
         # Passing batches of predictions/references is recomended to avoid repeated model loading
         if self.bertscore_model is None:

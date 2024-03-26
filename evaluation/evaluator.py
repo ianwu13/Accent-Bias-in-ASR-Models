48bcc54ae79212d0cc548a04880300cd4ddac9de
Ian Wu <ianwu13@me.com>
2024-03-26 00:45:09 -0700

    debug typos

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -1,6 +1,7 @@
 from jiwer import wer
 from evaluate import load
 import jaro
+from typing import Union
 
 
 class Evaluator():
@@ -8,9 +9,13 @@ class Evaluator():
         self.bert_model = bert_model
         if preload_bertscore_model:
             self.bertscore_model = load('bertscore')
+        else:
+            self.bertscore_model = None
+
+        self.cer_metric = None
 
 
-    def wer(self, pred: str|list, ref: str|list) -> float:
+    def wer(self, pred: Union[str, list], ref: Union[str, list]) -> float:
         # Returns word error rate (WER) between some predicted text and a reference (true label) text
         if type(pred) == str and type(ref) == str:
             return wer(ref, pred)
@@ -20,7 +25,7 @@ class Evaluator():
             return None
 
 
-    def cer(self, pred: str|list, ref: str|list) -> float:
+    def cer(self, pred: Union[str, list], ref: Union[str, list]) -> float:
         # Returns character error rate (CER) between some predicted text and a reference (true label) text
         if self.cer_metric is None:
             self.cer_metric = load('cer')
@@ -33,7 +38,7 @@ class Evaluator():
             return None
 
 
-    def jaro_winkler(self, pred: str|list, ref: str|list) -> float:
+    def jaro_winkler(self, pred: Union[str, list], ref: Union[str, list]) -> float:
         # Returns character error rate (CER) between some predicted text and a reference (true label) text
         if type(pred) == str and type(ref) == str:
             pred = [pred]
@@ -42,7 +47,7 @@ class Evaluator():
         return [jaro.jaro_metric(p, r) for p, r in zip(pred, ref)]
 
 
-    def bertscore(self, pred: str|list, ref: str|list) -> float:
+    def bertscore(self, pred: Union[str, list], ref: Union[str, list]) -> float:
         # Returns bertscore between some predicted text and a reference (true label) text
         # Passing batches of predictions/references is recomended to avoid repeated model loading
         if self.bertscore_model is None:
